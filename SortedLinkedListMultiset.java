773cd8e74aaf6d5735f3684ee807b8132df7cf66
rmit-s3572164-farid-farzin <s3572164@student.rmit.edu.au>
2017-08-19 19:20:05 +1000

    Update SortedLinkedListMultiset.java

--- SortedLinkedListMultiset.java
+++ SortedLinkedListMultiset.java
@@ -3,12 +3,12 @@ import java.util.*;
 
 public class SortedLinkedListMultiset<T extends Comparable<T>> extends Multiset<T>
 {
-        /** A reference to the node at the head of the linked list */
-        protected Node mHead;
-        /** A reference to the node at the tail of the linked list */
-        protected Node mTail;
-        /** The length of the linked list */
-        protected int mLength;
+	/** A reference to the node at the head of the linked list */
+	protected Node mHead;
+	/** A reference to the node at the tail of the linked list */
+	protected Node mTail;
+	/** The length of the linked list */
+	protected int mLength;
 
 	public SortedLinkedListMultiset() {
 		// Implement me!
@@ -16,8 +16,8 @@ public class SortedLinkedListMultiset<T extends Comparable<T>> extends Multiset<
 		mTail = null;
 		mLength = 0;
 	} // end of SortedLinkedListMultiset()
-	
-	
+
+
 	public void add(T item) {
 		// Implement me!
 		Node currNode = mHead;
@@ -50,11 +50,12 @@ public class SortedLinkedListMultiset<T extends Comparable<T>> extends Multiset<
 				while (currNode != null){
 					T val = currNode.getValue();
 					Node nextNode = currNode.getNext();
-					if (nextNode == null) {
+					if (nextNode == null && !item.equals(val)) {
 						currNode.setNext(newNode);
 						newNode.setPrev(currNode);
 						mTail = newNode;
 						mLength++;
+						break;
 					}
 					else if (item.equals(val)) {
 						currNode.setInstances(currNode.getInstances()+1);
@@ -66,38 +67,93 @@ public class SortedLinkedListMultiset<T extends Comparable<T>> extends Multiset<
 						newNode.setNext(nextNode);
 						nextNode.setPrev(newNode);
 						mLength++;
+						break;
 					}
 					currNode = nextNode;
 				}
 			}
 		}
 	} // end of add()
-	
-	
+
+
 	public int search(T item) {
-		// Implement me!	
-		
-		// default return, please override when you implement this method
+		Node currNode = mHead;
+		while(!(currNode.getValue().compareTo(item)==0) && 
+				currNode.getNext()!=null){
+			currNode=currNode.getNext();
+		}
+		if(currNode.getValue().compareTo(item)==0)
+			return currNode.getInstances();		
 		return 0;
 	} // end of add()
-	
-	
+
+
 	public void removeOne(T item) {
-		// Implement me!
+		Node currNode = mHead;
+		Node next=null;
+		while(!(currNode.getValue().compareTo(item)==0) && 
+				currNode.getNext()!=null){
+			currNode=currNode.getNext();
+		}
+		if(currNode.getValue().compareTo(item)==0){
+			if(currNode.getInstances()!=1)
+				currNode.setInstances(currNode.getInstances()-1); 
+			else{
+				if(mHead.getValue().compareTo(item)==0){
+					if(mHead.getNext()!=null){
+						mHead=mHead.getNext();
+						if(mHead.getPrev()!=null)
+							mHead.setPrev(null);
+					} else 
+						mHead = null;
+				}else{
+					next = currNode.getNext();
+					currNode = currNode.getPrev();
+					currNode.setNext(next);
+					if(next!=null && currNode.getPrev()!=null)
+						currNode.getNext().setPrev(currNode);
+				}
+			}
+		}
+
 	} // end of removeOne()
-	
-	
+
+
 	public void removeAll(T item) {
-		// Implement me!
+		Node currNode = mHead;
+		Node next=null;
+		if(mHead.getValue().compareTo(item)==0){
+			if(mHead.getNext()!=null){
+				mHead=mHead.getNext();
+				if(mHead.getPrev()!=null)
+					mHead.setPrev(null);
+			} else 
+				mHead = null;
+		}
+		else{
+			while(!(currNode.getValue().compareTo(item)==0) && 
+					currNode!=null){
+				currNode=currNode.getNext();
+			}
+			if(currNode.getValue().compareTo(item)==0){
+				next = currNode.getNext();
+				currNode = currNode.getPrev();
+				currNode.setNext(next);
+				if(next!=null && currNode.getPrev()!=null)
+					currNode.getNext().setPrev(currNode);
+			}
+		}
+
+
 	} // end of removeAll()
-	
-	
+
+
 	/**
- 	* @name print
- 	* @type Method 
- 	* @descr Print a string representation of the multiset
- 	* @return String representation of the multiset
- 	*/ 	
+	 * @name print
+	 * @type Method 
+	 * @descr Print a string representation of the multiset
+	 * @return String representation of the multiset
+	 */ 	
 	public void print(PrintStream out) {
 		// Implement me!
 		Node currNode = mHead;
@@ -107,56 +163,56 @@ public class SortedLinkedListMultiset<T extends Comparable<T>> extends Multiset<
 		}
 	} // end of print()
 
-        /**
+	/**
 	 * @name Node
 	 * @type Class
-         * @descr Class which represents the individual elements which constitute a multiset
- 	 * @author s3586372 Rahul Raghavan, Farid Farzin
- 	 * @created 10-08-2017
- 	 */                                                                                                 
-        private class Node
-        {
-                /** The value of the Node */
-                private T mValue;
-                /** The reference to the next node */
-                private Node mNext;
-                /** The reference to the previous node */
-                private Node mPrev;
-                /** The number of instances of the value in the multiset */
-                private int mInstances;
+	 * @descr Class which represents the individual elements which constitute a multiset
+	 * @author s3586372 Rahul Raghavan, Farid Farzin
+	 * @created 10-08-2017
+	 */                                                                                                 
+	private class Node
+	{
+		/** The value of the Node */
+		private T mValue;
+		/** The reference to the next node */
+		private Node mNext;
+		/** The reference to the previous node */
+		private Node mPrev;
+		/** The number of instances of the value in the multiset */
+		private int mInstances;
 
-                /** constructor */
-                public Node(T item, int instances) {
-                        mValue = item;
-                        mNext = null;
-                        mPrev = null;
-                        mInstances = instances;
-                }
-                public T getValue() {
-                        return mValue;
-                }
-                public Node getNext() {
-                        return mNext;
-                }
-                public Node getPrev() {
-                        return mPrev;
-                }
-                public int getInstances() {
-                        return mInstances;
-                }
-                public void setValue(T val) {
-                        mValue = val;
-                }
-                public void setNext(Node next) {
-                        mNext = next;
-                }
-                public void setPrev(Node prev) {
-                        mPrev = prev;
-                }
-                public void setInstances(int inst) {
-                        mInstances = inst;
-                }
-        }
+		/** constructor */
+		public Node(T item, int instances) {
+			mValue = item;
+			mNext = null;
+			mPrev = null;
+			mInstances = instances;
+		}
+		public T getValue() {
+			return mValue;
+		}
+		public Node getNext() {
+			return mNext;
+		}
+		public Node getPrev() {
+			return mPrev;
+		}
+		public int getInstances() {
+			return mInstances;
+		}
+		public void setValue(T val) {
+			mValue = val;
+		}
+		public void setNext(Node next) {
+			mNext = next;
+		}
+		public void setPrev(Node prev) {
+			mPrev = prev;
+		}
+		public void setInstances(int inst) {
+			mInstances = inst;
+		}
+	}
 
-	
+
 } // end of class SortedLinkedListMultiset
